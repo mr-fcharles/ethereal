e924ffc2f5fa918d6b3c4738f4b8124d05bf04a6
mr-fcharles <francesco.carli94@gmail.com>
2020-04-22 17:32:04 +0200

    minimum required files

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def main():
+	"""Streamlit Quick Reference App 
+	 For Easy Reference and Quick Ref
+
+	 """
+
+	st.title('claudia sei bellissima')
+
+
+if __name__ == '__main__':
+	main()
+
