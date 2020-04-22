2819908067c09ca6d93bda07921bdcb26f043308
mr-fcharles <francesco.carli94@gmail.com>
2020-04-22 18:07:35 +0200

    new version

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 
+
 def main():
 	"""Streamlit Quick Reference App 
 	 For Easy Reference and Quick Ref
 
 	 """
 
-	st.title('claudia sei bellissima')
+	st.title('Ciao schiappe')
+
+	from PIL import Image 
+	img = Image.open("otaria.jpg")
+	st.image(img,use_column_width=True,caption='Streamlit Images')
 
 
 if __name__ == '__main__':
